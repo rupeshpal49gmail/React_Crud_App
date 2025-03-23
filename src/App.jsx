6896bb02b8e0a6af1b8f3c399779903e61dc3267
Rupesh Pal <rupeshpal49@gmail.com>
2025-03-24 00:46:34 +0530

    add save, clear,and  edit section

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,65 @@ import { StudentDetails } from "./StudentDetails";
 function App() {
   const [data, setData] = useState([]);
 
+  // Bind Studentdetails file to render start //
+
   useEffect(() => {
     setData(StudentDetails);
   }, []);
+
+  // Bind Studentdetails file to render end //
+
+  // Handle edit section start //
+  const handleEdit = (id) => {
+    alert(id);
+  };
+  // Handle edit section end //
+
+  // Handle delete section start //
+  const handleDelete = (id) => {
+    if (id > 0) {
+      if (window.confirm("Are you sure to delete this item?")) {
+        const dt = data.filter((item) => item.id !== id);
+        setData(dt);
+      }
+    }
+  };
+
+  // Handle delete section end //
+
   return (
     <>
-      <table>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>
+          <label>Student Name</label>
+          <input type="text" placeholder="Student name" />
+        </div>
+        <div>
+          <label>Age</label>
+          <input type="number" placeholder="Age" />
+        </div>
+        <div>
+          <label>Roll number</label>
+          <input type="number" placeholder="Roll number" />
+        </div>
+        <div>
+          <button className="btn btn-primary" onClick={() => handleSave(id)}>
+            Save
+          </button>
+          <button className="btn btn-primary" onClick={() => handleClear(id)}>
+            Clear
+          </button>
+        </div>
+      </div>
+      <table className="table table-hover">
         <thead>
           <tr>
             <td>Sr. No</td>
             <td>Id</td>
             <td>Student Name</td>
+            <td>Age</td>
             <td>Roll Number</td>
+            <td>Action</td>
           </tr>
         </thead>
         <tbody>
@@ -26,7 +73,22 @@ function App() {
                 <td>{index + 1}</td>
                 <td>{item.id}</td>
                 <td>{item.studentName}</td>
+                <td>{item.studentAge}</td>
                 <td>{item.rollNumber}</td>
+                <td>
+                  <button
+                    className="btn btn-primary mx-4"
+                    onClick={() => handleEdit(item.id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             );
           })}
--- src/StudentDetails.jsx
+++ src/StudentDetails.jsx
@@ -2,36 +2,43 @@ export let StudentDetails = [
   {
     id: 1,
     studentName: "Arun",
+    studentAge: 20,
     rollNumber: 101,
   },
   {
     id: 2,
     studentName: "Hari",
+    studentAge: 23,
     rollNumber: 102,
   },
   {
     id: 3,
     studentName: "Shivam",
+    studentAge: 20,
     rollNumber: 103,
   },
   {
     id: 4,
     studentName: "Ravi",
+    studentAge: 22,
     rollNumber: 104,
   },
   {
     id: 5,
     studentName: "Satyam",
+    studentAge: 21,
     rollNumber: 105,
   },
   {
     id: 6,
     studentName: "Golu",
+    studentAge: 20,
     rollNumber: 106,
   },
   {
     id: 7,
     studentName: "Pritam",
+    studentAge: 22,
     rollNumber: 107,
   },
 ];
